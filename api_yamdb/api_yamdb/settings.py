ce1b2e5686380dfd14d79e66a1a0eaf205fc6e0a
Владислав Бунин <gaius.capito@gmail.com>
2023-06-01 22:00:14 +0300

    6 Правки по ревью

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -9,7 +9,7 @@ SECRET_KEY = os.getenv('SECRET_KEY'),
 
 DEBUG = False
 
-ALLOWED_HOSTS = list(os.getenv('ALLOWED_HOSTS', default='127.0.0.1').split())
+ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
